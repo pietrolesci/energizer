db3a5b93dea5f75a7f6a0861da39fdd3933268c1
pietrolesci <pietrolesci@outlook.com>
2021-12-20 22:21:47 +0100

    enhance active datamodule

--- energizer/data/__init__.py
+++ energizer/data/__init__.py
@@ -1,3 +1,4 @@
+from energizer.data.active_datamodule import ActiveDataModule
 from energizer.data.active_dataset import ActiveDataset
 
-__all__ = ["ActiveDataset"]
+__all__ = ["ActiveDataset", "ActiveDataModule"]
