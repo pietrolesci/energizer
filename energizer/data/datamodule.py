8f3614a6340d3f6143a0fc55859cf25d81865a59
Pietro Lesci <61748653+pietrolesci@users.noreply.github.com>
2021-12-26 15:50:02 +0100

    rename datamodule

--- energizer/data/datamodule.py
+++ energizer/data/datamodule.py
@@ -1,13 +1,15 @@
+from functools import partial
 from typing import Callable, List, Optional, Union
 
 import datasets
 import numpy as np
 from numpy.random import default_rng
 from pytorch_lightning import LightningDataModule
+from pytorch_lightning.trainer.states import RunningStage
 from pytorch_lightning.utilities.exceptions import MisconfigurationException
 from torch.utils.data import BatchSampler, DataLoader, Dataset, Sampler, SequentialSampler
 
-from energizer.data.active_dataset import ActiveDataset, EnergizerSubset
+from energizer.data.dataset import ActiveDataset, EnergizerSubset
 
 
 class ActiveDataModule(LightningDataModule):
@@ -21,7 +23,7 @@ class ActiveDataModule(LightningDataModule):
         val_dataset: Optional[Union[Dataset, datasets.Dataset]] = None,
         val_split: Optional[float] = None,
         test_dataset: Optional[Union[Dataset, datasets.Dataset]] = None,
-        min_steps_per_epoch: Optional[int] = None,
+        predict_dataset: Optional[Union[Dataset, datasets.Dataset]] = None,
         batch_size: int = 1,
         shuffle: Optional[bool] = False,
         num_workers: int = 0,
@@ -38,25 +40,25 @@ class ActiveDataModule(LightningDataModule):
             train_dataset (Union[Dataset, datasets.Dataset]): The dataset to use for training. This
                 will be transformed into an `ActiveDataset`. If `val_split` is specified, validation set will
                 be active too and will be created as a portion of the labelled instances in each iteration.
-            initial_labels (Optional[Union[int, List[int]]]): The indices to randomly label prior to starting
-                training.
+            initial_labels (Optional[Union[int, List[int]]]): The instances to label prior to starting the
+                training loop.  If it is a `List[int]` it is interpreteed as the actual indices of the instances
+                to label. If it is an `int` it will be the size of the indeces to randomly draw from the pool
+                and label.
             val_dataset (Optional[Union[Dataset, datasets.Dataset]]): The validation dataset.
             val_split (Optional[float]): The proportion of labelled data to add to the validation set at each
                 labelling iteration.
             test_dataset (Optional[Union[Dataset, datasets.Dataset]]): The test dataset.
-            min_steps_per_epoch (Optional[int]): Minumum number of steps per epoch. Especially in the beginning
-                of active learning when there are a few labelled instances, this arguments allows the user to
-                train for a minimum number of iterations by resampling the available data.
-            batch_size (Optional[int]): The batch size used to train the model. The batch size for evaluating the
+            test_dataset (Optional[Union[Dataset, datasets.Dataset]]): The prediction dataset.
+            batch_size (int): The batch size used to train the model. The batch size for evaluating the
                 model on the pool is automatically set by the `ActiveTrainer` and exploits the maximum available
                 bandwidth.
             shuffle (Optional[bool]): Whether to shuffle the data at each training iteration.
-            num_workers (Optional[int]): How many subprocesses to use for data loading. ``0`` means that the data
+            num_workers (int): How many subprocesses to use for data loading. ``0`` means that the data
                 will be loaded in the main process.
             collate_fn (Optional[Callable]): Merges a sequence of samples to form a mini-batch of Tensor(s).
-            pin_memory (Optional[bool]): TBD
-            drop_last (Optional[bool]): TBD
-            persistent_workers (Optional[bool]): TBD
+            pin_memory (bool): TBD
+            drop_last (bool): TBD
+            persistent_workers (bool): TBD
             seed (Optional[int]): Seed used to shuffle the data if `shuffle` is True and to split data in the train
                 and validation set if `val_split` is True.
         """
@@ -64,7 +66,6 @@ class ActiveDataModule(LightningDataModule):
         self.num_classes = num_classes
         self.initial_labels = initial_labels
         self.val_split = val_split
-        self.min_steps_per_epoch = min_steps_per_epoch
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.num_workers = num_workers
@@ -77,6 +78,10 @@ class ActiveDataModule(LightningDataModule):
         self._active_dataset = ActiveDataset(train_dataset, self.seed)
         self._val_dataset = val_dataset
         self._test_dataset = test_dataset
+        self._predict_dataset = predict_dataset
+
+        # this will be overwritten in the ActiveLearningLoop
+        self._min_steps_per_epoch = None
 
         # check val_dataset and val_split
         if self._val_dataset and self.val_split:
@@ -95,17 +100,38 @@ class ActiveDataModule(LightningDataModule):
         if self.initial_labels is not None:
             if isinstance(self.initial_labels, int):
                 self.initial_labels = self._active_dataset.sample_pool_idx(self.initial_labels)
-            self.label(self.initial_labels, self.val_split)  # type: ignore
+            self.label(self.initial_labels)  # type: ignore
 
         if not self._active_dataset.has_labelled_data:
             print("Cold-starting: The training dataset does not contain any labelled instance.")
 
+        # setup dataloaders
+        self.train_dataloader = partial(self._make_dataloader, self.train_dataset, RunningStage.TRAINING)
+        self.pool_dataloader = partial(self._make_dataloader, self.pool_dataset, RunningStage.PREDICTING)
+
+        if self.val_dataset:
+            self.val_dataloader = partial(self._make_dataloader, self.val_dataset, RunningStage.VALIDATING)
+
+        if self.test_dataset:
+            self.test_dataloader = partial(self._make_dataloader, self.test_dataset, RunningStage.TESTING)
+
+        if self.predict_dataset:
+            self.predict_dataloader = partial(self._make_dataloader, self.predict_dataset, RunningStage.PREDICTING)
+
         self.save_hyperparameters("num_classes", "batch_size", "shuffle", "val_split", "initial_labels", "seed")
 
-    def _make_dataloader(self, dataset: Union[EnergizerSubset, Dataset, datasets.Dataset]) -> DataLoader:
-        if isinstance(dataset, EnergizerSubset):
+    def prepare_data(self) -> None:
+        pass
+
+    def setup(self, stage=None) -> None:
+        pass
+
+    def _make_dataloader(
+        self, dataset: Union[EnergizerSubset, Dataset, datasets.Dataset], running_stage: RunningStage
+    ) -> DataLoader:
+        if running_stage in (RunningStage.TRAINING, RunningStage.VALIDATING) and isinstance(dataset, EnergizerSubset):
             sampler = FixedLengthSampler(
-                dataset, n_steps=self.min_steps_per_epoch, shuffle=self.shuffle, seed=self.seed
+                dataset, n_steps=self._min_steps_per_epoch, shuffle=self.shuffle, seed=self.seed
             )
         else:
             sampler = SequentialSampler(dataset)  # type: ignore
@@ -122,26 +148,6 @@ class ActiveDataModule(LightningDataModule):
             pin_memory=self.pin_memory,
             persistent_workers=self.persistent_workers,
         )
-
-    def train_dataloader(self) -> DataLoader:
-        """Returns the train dataloader."""
-        return self._make_dataloader(self.train_dataset)
-
-    def val_dataloader(self) -> Optional[DataLoader]:  # type: ignore
-        """Returns the validation dataloader."""
-        if not self.val_dataset:
-            return None
-        return self._make_dataloader(self.val_dataset)
-
-    def test_dataloader(self) -> Optional[DataLoader]:  # type: ignore
-        """Returns the test dataloader."""
-        if not self.test_dataset:
-            return None
-        return self._make_dataloader(self.test_dataset)
-
-    def pool_dataloader(self) -> DataLoader:
-        """Returns the pool dataloader."""
-        return self._make_dataloader(self.pool_dataset)
 
     @property
     def train_dataset(self) -> EnergizerSubset:
@@ -170,6 +176,11 @@ class ActiveDataModule(LightningDataModule):
     def test_dataset(self) -> Optional[Union[Dataset, datasets.Dataset]]:
         """Returns the test dataset from the underlying active dataset."""
         return self._test_dataset
+
+    @property
+    def predict_dataset(self) -> Optional[Union[Dataset, datasets.Dataset]]:
+        """Returns the predict dataset from the underlying active dataset."""
+        return self._predict_dataset
 
     @property
     def train_size(self) -> int:
@@ -222,10 +233,13 @@ class ActiveDataModule(LightningDataModule):
 
         Args:
             size (int): The number of indices to sample from the pool. Must be 0 < size <= pool_size
+
+        Returns:
+            The list of indices from the pool to label.
         """
         return self._active_dataset.sample_pool_idx(size)
 
-    def label(self, pool_idx: Union[int, List[int]], val_split: Optional[float] = None) -> None:
+    def label(self, pool_idx: Union[int, List[int]]) -> None:
         """Convenience method that calls the underlying self._active_dataset.label method.
 
         Adds instances to the `labelled_dataset`.
@@ -237,7 +251,7 @@ class ActiveDataModule(LightningDataModule):
             val_split (Optional[float]): The proportion of the number of instances to add to the validation set.
                 This is considered only when `len(pool_idx) > 1`.
         """
-        self._active_dataset.label(pool_idx, val_split)
+        self._active_dataset.label(pool_idx, self.val_split)
 
 
 class FixedLengthSampler(Sampler):
