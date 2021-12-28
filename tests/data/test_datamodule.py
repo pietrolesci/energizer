eeee860ea445f14ea75031b2f9302a015770b477
pietrolesci <pietrolesci@outlook.com>
2021-12-28 19:08:42 +0100

    add tests for dataset and datamodule

--- tests/data/test_datamodule.py
+++ tests/data/test_datamodule.py
@@ -1,6 +1,9 @@
 import pytest
+from pytorch_lightning.utilities.exceptions import MisconfigurationException
+from torch.utils.data.sampler import SequentialSampler
 
-from energizer.data import ActiveDataset
+from energizer.data import ActiveDataModule
+from energizer.data.datamodule import FixedLengthSampler
 
 
 @pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
@@ -8,7 +11,9 @@ def test_len(dataset_arg):
     """Test that measures of length are consistent."""
 
     # no instances
-    ads = ActiveDataset(dataset_arg)
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
+    ads.prepare_data()  # useless: just pass but for coverage
+    ads.setup()  # useless: just pass but for coverage
     assert ads.total_labelled_size == ads.train_size + ads.val_size
     assert len(ads.train_dataset) == ads.train_size == ads.val_size == ads.total_labelled_size == 0
     assert len(dataset_arg) == len(ads.pool_dataset) == ads.pool_size
@@ -18,12 +23,13 @@ def test_len(dataset_arg):
     ads.label(0)
     assert ads.total_labelled_size == ads.train_size + ads.val_size
     assert len(ads.train_dataset) == ads.train_size == ads.total_labelled_size == 1
-    assert len(ads.val_dataset) == ads.val_size == 0
+    assert ads.val_dataset is None
     assert len(dataset_arg) - ads.total_labelled_size == len(ads.pool_dataset) == ads.pool_size
     assert len(dataset_arg) == ads.total_labelled_size + ads.pool_size
 
     # one instance in the train dataset and one in the val dataset
-    ads.label([0, 1], val_split=0.5)
+    ads.val_split = 0.5  # hack
+    ads.label([0, 1])
     assert ads.total_labelled_size == ads.train_size + ads.val_size
     assert len(ads.train_dataset) == ads.train_size == 2
     assert len(ads.val_dataset) == ads.val_size == 1
@@ -33,8 +39,8 @@ def test_len(dataset_arg):
 
 @pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
 def test_indexing(dataset_arg):
-    """Test that ActiveDataset is not indexable directly."""
-    ads = ActiveDataset(dataset_arg)
+    """Test that ActiveDataModule is not indexable directly."""
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
     with pytest.raises(TypeError):
         assert ads[0]
 
@@ -42,7 +48,7 @@ def test_indexing(dataset_arg):
 @pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
 def test_labelling(dataset_arg):
     """Test that labelling changes all the required states."""
-    ads = ActiveDataset(dataset_arg)
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
     len_dataset_arg = len(dataset_arg)
 
     assert ads.last_labelling_step == 0
@@ -76,7 +82,7 @@ def test_labelling(dataset_arg):
 @pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
 def test_labelling_multiple_indices(dataset_arg):
     """Test labelling multiple instances at once."""
-    ads = ActiveDataset(dataset_arg)
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
     pool_ids = [0, 8, 7]  # they are the first to be labelled so correspond to ids in oracle
     ads.label(pool_ids)
 
@@ -88,15 +94,15 @@ def test_labelling_duplicates(dataset_arg):
     """Test that labelling duplicate indices results in a single instance to be labelled."""
 
     # check behaviour when batch of indices contains
-    ads = ActiveDataset(dataset_arg)
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
     pool_ids = [0, 0]  # they are the first to be labelled so correspond to ids in oracle
     ads.label(pool_ids)
     assert ads.train_size == 1
 
     # check behaviour when batch of indices contains
-    ads = ActiveDataset(dataset_arg)
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, val_split=0.5)
     pool_ids = [0, 0, 1]  # they are the first to be labelled so correspond to ids in oracle
-    ads.label(pool_ids, val_split=0.5)
+    ads.label(pool_ids)
     assert ads.train_size == ads.val_size == 1
 
 
@@ -105,29 +111,29 @@ def test_labelling_val_split(dataset_arg):
     """Test that labelling with val_split works."""
 
     # check split works
-    ads = ActiveDataset(dataset_arg)
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, val_split=0.5)
     pool_ids = [0, 1]  # they are the first to be labelled so correspond to ids in oracle
-    ads.label(pool_ids, val_split=0.5)
+    ads.label(pool_ids)
     assert ads.train_size == ads.val_size == 1
 
     # check that val_split receives at least 1 instance when there are two labelled instances
     # and the probability is too small that it randomly would receive just one
-    ads = ActiveDataset(dataset_arg)
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, val_split=0.0001)
     pool_ids = [0, 1]  # they are the first to be labelled so correspond to ids in oracle
-    ads.label(pool_ids, val_split=0.0001)
+    ads.label(pool_ids)
     assert ads.train_size == ads.val_size == 1
 
     # check behaviour when there is only one instance (bonus: using a duplicate)
-    ads = ActiveDataset(dataset_arg)
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, val_split=0.99)
     pool_ids = [0, 0]  # they are the first to be labelled so correspond to ids in oracle
-    ads.label(pool_ids, val_split=0.99)
+    ads.label(pool_ids)
     assert ads.train_size == 1
 
 
 @pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
 def test_reset_at_labelling_step(dataset_arg):
     """Test that resetting the labelling steps sets the correct states."""
-    ads = ActiveDataset(dataset_arg)
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
     len_dataset_arg = len(dataset_arg)
 
     ads.label(0)  # label first
@@ -173,7 +179,7 @@ def test_reset_at_labelling_step(dataset_arg):
 
 @pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
 def test_sample_pool_indices(dataset_arg):
-    ads = ActiveDataset(dataset_arg)
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
 
     with pytest.raises(ValueError):
         assert ads.sample_pool_idx(-1)
@@ -190,7 +196,7 @@ def test_sample_pool_indices(dataset_arg):
 
 @pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
 def test_curriculum(dataset_arg):
-    ads = ActiveDataset(dataset_arg)
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
 
     for _ in range(5):
         ads.label(0)
@@ -199,8 +205,76 @@ def test_curriculum(dataset_arg):
 
 
 @pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
-def test_pool_to_oracle(dataset_arg):
-    ads = ActiveDataset(dataset_arg)
+def test_initial_labelling(dataset_arg):
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
+    assert ads.train_size == 0
 
-    for i in range(ads.pool_size):
-        assert [i] == ads._pool_to_oracle(i)
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, initial_labels=[0])
+    assert ads.train_size == 1
+
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, initial_labels=2)
+    assert ads.train_size == 2
+
+    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, initial_labels=2, val_split=0.5)
+    assert ads.train_size == ads.val_size == 1
+
+
+@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
+def test_dataloader_len(dataset_arg):
+    for batch_size in range(1, len(dataset_arg) + 1):
+        ads = ActiveDataModule(
+            num_classes=2,
+            train_dataset=dataset_arg,
+            initial_labels=2,
+            batch_size=batch_size,
+        )
+        assert ads.train_dataloader().batch_size is None
+        assert ads.train_dataloader().batch_sampler.batch_size == batch_size
+        assert len(ads.train_dataloader().batch_sampler) == len(ads.train_dataloader())
+
+    # min_steps_per_epoch
+    for shuffle in (True, False):
+        ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, initial_labels=2, shuffle=shuffle)
+        ads._min_steps_per_epoch = 1
+        assert len(ads.train_dataloader().batch_sampler) == len(ads.train_dataloader()) == 2
+        for _ in range(2):
+            assert next(iter(ads.train_dataloader()))
+
+        ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, initial_labels=2, shuffle=shuffle)
+        ads._min_steps_per_epoch = 10
+        assert len(ads.train_dataloader().batch_sampler) == len(ads.train_dataloader()) == 10
+        for _ in range(10):
+            assert next(iter(ads.train_dataloader()))
+
+
+@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
+def test_sampler_type(dataset_arg):
+    ads = ActiveDataModule(
+        num_classes=2,
+        train_dataset=dataset_arg,
+        test_dataset=dataset_arg,
+        predict_dataset=dataset_arg,
+        val_dataset=dataset_arg,
+        initial_labels=2,
+        batch_size=1,
+    )
+
+    assert isinstance(ads.train_dataloader().batch_sampler.sampler, FixedLengthSampler)
+    assert isinstance(ads.pool_dataloader().batch_sampler.sampler, SequentialSampler)
+    assert isinstance(ads.val_dataloader().batch_sampler.sampler, SequentialSampler)
+    assert isinstance(ads.test_dataloader().batch_sampler.sampler, SequentialSampler)
+    assert isinstance(ads.predict_dataloader().batch_sampler.sampler, SequentialSampler)
+
+
+@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
+def test_raise_errors(dataset_arg):
+    for i in (-0.5, 1.0):
+        with pytest.raises(MisconfigurationException):
+            ActiveDataModule(num_classes=2, train_dataset=dataset_arg, val_split=i)
+
+    with pytest.raises(MisconfigurationException):
+        ActiveDataModule(num_classes=2, train_dataset=dataset_arg, val_split=i, val_dataset=dataset_arg)
+
+    with pytest.raises(RuntimeError):
+        ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
+        next(iter(ads.train_dataloader()))
