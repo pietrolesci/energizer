ae2d626b3219dbe9cd2dfbbd42ec5c3858c45d28
pietrolesci <pietrolesci@outlook.com>
2022-02-09 16:15:34 +0000

    update

--- energizer/loops/__init__.py
+++ energizer/loops/__init__.py
@@ -1,3 +1,5 @@
 from energizer.loops.active_loop import ActiveLearningLoop
+from energizer.loops.pool_epoch_loop import PoolEpochLoop
+from energizer.loops.pool_loop import PoolLoop
 
-__all__ = ["ActiveLearningLoop"]
+__all__ = ["ActiveLearningLoop", "PoolLoop", "PoolEpochLoop"]
