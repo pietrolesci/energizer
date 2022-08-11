903fa87ded21a2733ea312a1fe5d5ec3ce9ad945
Pietro Lesci <61748653+pietrolesci@users.noreply.github.com>
2022-08-11 19:45:11 +0200

    add datamodule to root imports

--- energizer/__init__.py
+++ energizer/__init__.py
@@ -1,9 +1,10 @@
 """Top-level package for Pytorch-Energizer."""
 
+from energizer.data.datamodule import ActiveDataModule
 from energizer.mixin.base import DeterministicMixin, MCDropoutMixin
 from energizer.trainer import Trainer
 
 __author__ = """Pietro Lesci"""
 __email__ = 'pietrolesci@outlook.com'
 __version__ = '0.1.0'
-__all__ = ["DeterministicMixin", "MCDropoutMixin", "Trainer"]
+__all__ = ["DeterministicMixin", "MCDropoutMixin", "Trainer", "ActiveDataModule"]
