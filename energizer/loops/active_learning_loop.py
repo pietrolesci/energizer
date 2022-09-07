f5b385040b9bf141dd0ccf4eb4dc1f1b13fb3b21
pietrolesci <pietrolesci@outlook.com>
2022-09-07 16:26:39 +0200

    Update internals and stabilize API (#10)
    * deprecate ActiveDataset
    
    * add datamodule to root imports
    
    * move utilities in their respective modules
    
    * update readme
    
    * update dependencies
    
    * remove old implementations
    
    * lint
    
    * add ActiveDatamodule2
    
    * move accumulator to loop module
    
    * fix callbacks
    
    * firs commit of the refactor
    
    * update
    
    * update readme
    
    * rename notebook
    
    * add text classification example
    
    * fix accumulator metric
    
    * fix accumulator loop
    
    * fix accumulator loop
    
    * rename variable
    
    * update poetry lock
    
    * update loops
    
    * upgrade deps
    
    * add logger
    
    * restructure
    
    * restructure
    
    * reformat
    
    * better logging
    
    * add faiss
    
    * search index (first)
    
    * random_anchors_strategy
    
    * linting
    
    * clean api and start working on all strategies
    
    * add strategies
    
    * add acquisition functions
    
    * change logging
    
    * revamp tests
    
    * update readme
    
    * add examples
    
    * formatting
    
    * examples

--- energizer/loops/active_learning_loop.py
+++ energizer/loops/active_learning_loop.py
@@ -1,7 +1,10 @@
 from copy import deepcopy
-from typing import List, Optional
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
 
 import torch
+import yaml
+from pandas import DataFrame
 from pytorch_lightning.loops.dataloader.evaluation_loop import EvaluationLoop
 from pytorch_lightning.loops.fit_loop import FitLoop
 from pytorch_lightning.loops.loop import Loop
@@ -9,9 +12,45 @@ from pytorch_lightning.loops.utilities import _is_max_limit_reached
 from pytorch_lightning.trainer.connectors.logger_connector.result import _ResultCollection
 from pytorch_lightning.trainer.progress import Progress
 from pytorch_lightning.trainer.states import TrainerFn
-from pytorch_lightning.utilities.distributed import rank_zero_only
+from pytorch_lightning.utilities.apply_func import apply_to_collection
+from pytorch_lightning.utilities.types import _EVALUATE_OUTPUT
 
-from energizer.loops.pool_loop import PoolEvaluationLoop
+from energizer.utilities.logger import logger
+from energizer.utilities.types import tensor_to_python
+
+
+@dataclass
+class LabellingIterOutputs:
+    labelling_iter: Optional[int] = None
+    indices: Optional[List[int]] = field(default_factory=list)
+    test_outputs: Optional[_EVALUATE_OUTPUT] = field(default_factory=list)
+    pool_outputs: Optional[_EVALUATE_OUTPUT] = field(default_factory=list)
+    data_stats: Optional[Dict[str, int]] = field(default_factory=dict)
+
+
+class LabellingOutputsList(list):
+    def to_pandas(self) -> DataFrame:
+        return DataFrame(
+            data=[(out.data_stats["train_size"], *out.test_outputs[0].values()) for out in self],
+            columns=("train_size", *self[0].test_outputs[0].keys()),
+        )
+
+    def __repr__(self) -> str:
+        tab = "    "
+        return f"{self.__class__.__name__}(\n{tab}" + f",\n{tab}".join(map(str, self)) + ",\n)"
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    def append(self, outputs: LabellingIterOutputs) -> None:
+        outputs = self._prepare_outputs(outputs)
+        return super().append(outputs)
+
+    def _prepare_outputs(self, outputs: LabellingIterOutputs) -> LabellingIterOutputs:
+        args = (torch.Tensor, tensor_to_python, "cpu")
+        outputs.test_outputs = apply_to_collection(outputs.test_outputs, *args)
+        outputs.pool_outputs = apply_to_collection(outputs.pool_outputs, *args)
+        return outputs
 
 
 class ActiveLearningLoop(Loop):
@@ -34,13 +73,19 @@ class ActiveLearningLoop(Loop):
         self.epoch_progress = Progress()
 
         # pool evaluation loop
-        self.pool_loop: Optional[PoolEvaluationLoop] = None
+        self.pool_loop: Optional[Loop] = None
 
         # fit after each labelling session
         self.fit_loop: Optional[FitLoop] = None
 
         # test after labelling and fitting
         self.test_loop: Optional[EvaluationLoop] = None
+
+        self._outputs: LabellingOutputsList = LabellingOutputsList()
+
+    @property
+    def can_run_testing(self) -> bool:
+        return self.test_after_labelling and self.trainer.datamodule.test_dataloader() is not None
 
     @property
     def _results(self) -> _ResultCollection:
@@ -58,8 +103,8 @@ class ActiveLearningLoop(Loop):
     @property
     def done(self) -> bool:
         """Evaluates when to leave the loop."""
-        # `processed` is increased before `on_train_epoch_end`, the hook where checkpoints are typically saved.
-        # we use it here because the checkpoint data won't have `completed` increased yet
+        # TODO: add stopping condition based on satisfactory level of a
+        # performance metric. Potentially do this via a callback
 
         # stop if no more data to label are available
         stop_no_data_to_label = not (
@@ -80,7 +125,7 @@ class ActiveLearningLoop(Loop):
     def skip(self) -> bool:
         return self.done
 
-    def connect(self, pool_loop: PoolEvaluationLoop, fit_loop: FitLoop, test_loop: EvaluationLoop) -> None:
+    def connect(self, pool_loop: Loop, fit_loop: FitLoop, test_loop: EvaluationLoop) -> None:
         self.pool_loop = pool_loop
         self.fit_loop = fit_loop
         self.test_loop = test_loop
@@ -93,24 +138,22 @@ class ActiveLearningLoop(Loop):
 
     def on_run_start(self) -> None:
         """Store the original weights of the model."""
-
         self.epoch_progress.current.completed = self.epoch_progress.current.processed
 
-        # make a copy of the initial state of the model to reset the weights
+        # make a copy of the initial state of the underlying model
+        # in `trainer.active_fit` the underlying model is passed,
+        # ie `self._run(model.model, ...)``
         if self.reset_weights:
             self.lightning_module_state_dict = deepcopy(self.trainer.lightning_module.state_dict())
 
-        self._results.to(device=self.trainer.lightning_module.device)
+        # set query_strategy as lightning_module to access hooks
+        self.trainer.set_lightning_module(use_query_strategy=True)
 
+        self._results.to(device=self.trainer.lightning_module.device)
         self.trainer._call_callback_hooks("on_active_learning_start")
         self.trainer._call_lightning_module_hook("on_active_learning_start")
 
     def on_advance_start(self) -> None:
-
-        # TODO: check if this is needed
-        # reset outputs here instead of in `reset` as they are not accumulated between epochs
-        self._outputs = []
-
         self.epoch_progress.increment_ready()
 
         # TODO: check this connector
@@ -121,30 +164,41 @@ class ActiveLearningLoop(Loop):
 
         self.epoch_progress.increment_started()
 
+        self._print_separator_line()
+
     def advance(self) -> None:  # type: ignore[override]
         """Runs the active learning loop: training, testing, and pool evaluation."""
-        if self.trainer.datamodule.has_labelled_data:
-            self._reset_fitting()  # required to reset the tracking stage
-            self.fit_loop.run()
-            # reset counters so that epochs always starts from 0
-            self.fit_loop.epoch_progress.reset()
+        # TODO: add `with self.trainer.profiler.profile("<some_step>"):`
 
-            if self.test_after_labelling:
-                self._reset_testing()  # required to reset the tracking stage
-                self.test_loop.run()
+        outputs = LabellingIterOutputs(
+            labelling_iter=self.epoch_progress.current.completed,
+            data_stats=self.trainer.datamodule.get_statistics(),
+        )
+
+        if self.trainer.datamodule.has_labelled_data:
+            self._reset_fitting()
+            self.fit_loop.run()
+
+        if self.can_run_testing:
+            # TODO: check that test_after_labelling and test_dl is present
+            # TODO: check it's using the last checkpoint
+            self._reset_testing()
+            outputs.test_outputs = self.test_loop.run()
+            self.test_loop._print_results(outputs.test_outputs, "test")
 
         if self.trainer.datamodule.has_unlabelled_data:
-            self._reset_evaluating_pool()  # requires to reset the tracking stage.
-            _, indices = self.pool_loop.run()
-            self.labelling_loop(indices)
+            self._reset_evaluating_pool()
+            outputs.pool_outputs, indices = self.pool_loop.run()
+            outputs.indices = indices
+            logger.info(f"Queried {len(indices)} instance")
+            self.label_datamodule(indices)
+
+        self._outputs.append(outputs)
 
     def on_advance_end(self) -> None:
         """Used to restore the original weights and the optimizers and schedulers states."""
         # TODO: check this connector
         self.trainer._logger_connector.epoch_end_reached()
-
-        if self.reset_weights:
-            self.trainer.lightning_module.load_state_dict(self.lightning_module_state_dict)
 
         self.epoch_progress.increment_processed()
 
@@ -159,23 +213,54 @@ class ActiveLearningLoop(Loop):
         # if fault tolerant is enabled and process has been notified, exit.
         self.trainer._exit_gracefully_on_signal()
 
-    def on_run_end(self) -> None:
+    def on_run_end(self) -> List[LabellingIterOutputs]:
+
         # enforce training at the end of acquisitions
+        self._print_separator_line(is_last=True)
+        outputs = LabellingIterOutputs(
+            labelling_iter=self.epoch_progress.current.completed,
+            data_stats=self.trainer.datamodule.get_statistics(),
+        )
         self._reset_fitting()
         self.fit_loop.run()
+        if self.can_run_testing:
+            self._reset_testing()
+            outputs.test_outputs = self.test_loop.run()
+            self.test_loop._print_results(outputs.test_outputs, "test")
 
+        self._outputs.append(outputs)
+        outputs, self._outputs = self._outputs, []
+
+        self.trainer.set_lightning_module(use_query_strategy=True)
         self.trainer._call_callback_hooks("on_active_learning_end")
         self.trainer._call_lightning_module_hook("on_active_learning_end")
 
+        return outputs
+
     def _reset_fitting(self) -> None:
+        # reset counters so that fit_loop is run again otherwise it does not run
+        self.fit_loop.epoch_progress.reset()
+
+        # puts the underlying module as "trainer.lightning_module"
+        self.trainer.set_lightning_module(use_query_strategy=False)
+
+        # maybe reset the weights
+        if self.reset_weights:
+            self.trainer.lightning_module.load_state_dict(self.lightning_module_state_dict)
+            logger.debug(f"{self.trainer.lightning_module.__class__.__name__} " "state dict has been re-initialized")
+
+        # resets the train/val dataloaders
         self.trainer.reset_train_dataloader()
         self.trainer.reset_val_dataloader()
+
+        # set up the states and enable grads
         self.trainer.state.fn = TrainerFn.FITTING
         self.trainer.training = True
         self.trainer.lightning_module.train()
         torch.set_grad_enabled(True)
 
     def _reset_testing(self) -> None:
+        self.trainer.set_lightning_module(use_query_strategy=False)
         self.trainer.reset_test_dataloader()
         self.trainer.state.fn = TrainerFn.TESTING
         self.trainer.testing = True
@@ -183,13 +268,25 @@ class ActiveLearningLoop(Loop):
         torch.set_grad_enabled(False)
 
     def _reset_evaluating_pool(self) -> None:
+        self.trainer.set_lightning_module(use_query_strategy=True)
         self.trainer.reset_pool_dataloader()
         self.trainer.state.fn = TrainerFn.TESTING
         self.trainer.testing = True
         self.trainer.lightning_module.eval()
         torch.set_grad_enabled(False)
 
-    @rank_zero_only
-    def labelling_loop(self, indices: List[int]) -> None:
+    def label_datamodule(self, indices: List[int]) -> Dict[str, int]:
         """This method changes the state of the underlying dataset."""
         self.trainer.datamodule.label(indices)
+        logger.info(f"Annotated {len(indices)} instances")
+        logger.info("New data statistics\n" f"{yaml.dump(self.trainer.datamodule.get_statistics())}")
+
+    """
+    Helpers
+    """
+
+    def _print_separator_line(self, is_last: bool = False) -> None:
+        if is_last:
+            print("Last fit_loop".center(72, "-"))
+            return
+        print(f"Labelling Iteration {self.epoch_progress.current.completed}".center(72, "-"), flush=True)
--- tests/unit/data/test_datamodule.py
+++ tests/unit/data/test_datamodule.py
@@ -1,280 +1,132 @@
 import pytest
-from pytorch_lightning.utilities.exceptions import MisconfigurationException
-from torch.utils.data.sampler import SequentialSampler
 
 from energizer.data import ActiveDataModule
-from energizer.data.datamodule import FixedLengthSampler
 
 
-@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
-def test_len(dataset_arg):
+def test_len(dataloader):
     """Test that measures of length are consistent."""
-
     # no instances
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
-    ads.prepare_data()  # useless: just pass but for coverage
-    ads.setup()  # useless: just pass but for coverage
-    assert ads.total_train_size == ads.train_size + ads.val_size
-    assert len(ads.train_dataset) == ads.train_size == ads.val_size == ads.total_train_size == 0
-    assert len(dataset_arg) == len(ads.pool_dataset) == ads.pool_size
-    assert len(dataset_arg) == ads.total_train_size + ads.pool_size
+    dm = ActiveDataModule(train_dataloader=dataloader)
+    assert len(dataloader.dataset) == dm.train_size + dm.pool_size
+    assert len(dataloader.dataset) == dm.total_data_size
+    pool_size = dm.pool_size
 
-    # one instance in the train dataset
-    ads.label(0)
-    assert ads.total_train_size == ads.train_size + ads.val_size
-    assert len(ads.train_dataset) == ads.train_size == ads.total_train_size == 1
-    assert ads.val_dataset is None
-    assert len(dataset_arg) - ads.total_train_size == len(ads.pool_dataset) == ads.pool_size
-    assert len(dataset_arg) == ads.total_train_size + ads.pool_size
-
-    # one instance in the train dataset and one in the val dataset
-    ads.val_split = 0.5  # hack
-    ads.label([0, 1])
-    assert ads.total_train_size == ads.train_size + ads.val_size
-    assert len(ads.train_dataset) == ads.train_size == 2
-    assert len(ads.val_dataset) == ads.val_size == 1
-    assert len(dataset_arg) - ads.total_train_size == len(ads.pool_dataset) == ads.pool_size
-    assert len(dataset_arg) == ads.total_train_size + ads.pool_size
+    dm.label(0)
+    assert len(dataloader.dataset) == dm.train_size + dm.pool_size
+    assert len(dataloader.dataset) == dm.total_data_size
+    assert dm.train_size == 1
+    assert dm.pool_size == pool_size - 1
 
 
-@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
-def test_indexing(dataset_arg):
+def test_indexing(dataloader):
     """Test that ActiveDataModule is not indexable directly."""
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
+    dm = ActiveDataModule(train_dataloader=dataloader)
     with pytest.raises(TypeError):
-        assert ads[0]
+        assert dm[0]
 
 
-@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
-def test_labelling(dataset_arg):
+def test_labelling(dataloader):
     """Test that labelling changes all the required states."""
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
-    len_dataset_arg = len(dataset_arg)
+    dm = ActiveDataModule(train_dataloader=dataloader)
+    len_dataset = len(dataloader.dataset)
 
-    assert ads.last_labelling_step == 0
-    assert ads.train_size == 0
-    assert ads.pool_size == len_dataset_arg
-    assert ads.has_labelled_data is False
-    assert ads.has_unlabelled_data is True
-    assert ads.train_dataset.indices == []
+    with pytest.raises(AssertionError):
+        dm.label("0")
 
-    for i in range(1, len_dataset_arg + 1):
-        ads.label(0)  # always label the first instance in the pool
+    with pytest.raises(ValueError):
+        dm.label(list(range(len_dataset + 100)))
 
-        assert ads.last_labelling_step == i
-        assert ads.train_size == i
-        assert ads.pool_size == len_dataset_arg - ads.train_size
-        assert ads.has_labelled_data is True
-        if i < len_dataset_arg:
-            assert ads.has_unlabelled_data is True
+    with pytest.raises(ValueError):
+        dm.label(int(1e6))
+
+    assert dm.last_labelling_step == 0
+    assert dm.train_size == 0
+    assert dm.pool_size == len_dataset
+    assert dm.has_labelled_data is False
+    assert dm.has_unlabelled_data is True
+    assert dm.train_dataset.indices == []
+
+    for i in range(1, len_dataset + 1):
+        dm.label(0)  # always label the first instance in the pool
+
+        assert dm.last_labelling_step == i
+        assert dm.train_size == i
+        assert dm.pool_size == len_dataset - dm.train_size
+        assert dm.has_labelled_data is True
+        if i < len_dataset:
+            assert dm.has_unlabelled_data is True
         else:
-            assert ads.has_unlabelled_data is False
-        assert ads.train_dataset.indices == list(range(i))
+            assert dm.has_unlabelled_data is False
+        assert dm.train_dataset.indices == list(range(i))
 
-    assert ads.last_labelling_step == len_dataset_arg
-    assert ads.train_size == len_dataset_arg
-    assert ads.pool_size == len_dataset_arg - ads.train_size
-    assert ads.has_labelled_data is True
-    assert ads.has_unlabelled_data is False
-    assert ads.train_dataset.indices == list(range(len_dataset_arg))
+    assert dm.last_labelling_step == len_dataset
+    assert dm.train_size == len_dataset
+    assert dm.pool_size == len_dataset - dm.train_size
+    assert dm.has_labelled_data is True
+    assert dm.has_unlabelled_data is False
+    assert dm.train_dataset.indices == list(range(len_dataset))
 
 
-@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
-def test_labelling_multiple_indices(dataset_arg):
+def test_labelling_multiple_indices(dataloader):
     """Test labelling multiple instances at once."""
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
+    dm = ActiveDataModule(train_dataloader=dataloader)
     pool_ids = [0, 8, 7]  # they are the first to be labelled so correspond to ids in oracle
-    ads.label(pool_ids)
+    dm.label(pool_ids)
 
-    assert ads.train_dataset.indices == sorted(pool_ids)
+    assert dm.train_dataset.indices == sorted(pool_ids)
 
 
-@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
-def test_labelling_duplicates(dataset_arg):
+def test_labelling_duplicates(dataloader):
     """Test that labelling duplicate indices results in a single instance to be labelled."""
 
     # check behaviour when batch of indices contains
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
+    dm = ActiveDataModule(train_dataloader=dataloader)
     pool_ids = [0, 0]  # they are the first to be labelled so correspond to ids in oracle
-    ads.label(pool_ids)
-    assert ads.train_size == 1
-
-    # check behaviour when batch of indices contains
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, val_split=0.5)
-    pool_ids = [0, 0, 1]  # they are the first to be labelled so correspond to ids in oracle
-    ads.label(pool_ids)
-    assert ads.train_size == ads.val_size == 1
+    dm.label(pool_ids)
+    assert dm.train_size == 1
 
 
-@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
-def test_labelling_val_split(dataset_arg):
-    """Test that labelling with val_split works."""
-
-    # check split works
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, val_split=0.5)
-    pool_ids = [0, 1]  # they are the first to be labelled so correspond to ids in oracle
-    ads.label(pool_ids)
-    assert ads.train_size == ads.val_size == 1
-
-    # check that val_split receives at least 1 instance when there are two labelled instances
-    # and the probability is too small that it randomly would receive just one
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, val_split=0.0001)
-    pool_ids = [0, 1]  # they are the first to be labelled so correspond to ids in oracle
-    ads.label(pool_ids)
-    assert ads.train_size == ads.val_size == 1
-
-    # check behaviour when there is only one instance (bonus: using a duplicate)
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, val_split=0.99)
-    pool_ids = [0, 0]  # they are the first to be labelled so correspond to ids in oracle
-    ads.label(pool_ids)
-    assert ads.train_size == 1
-
-
-@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
-def test_reset_at_labelling_step(dataset_arg):
+def test_reset_at_labelling_step(dataloader):
     """Test that resetting the labelling steps sets the correct states."""
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
-    len_dataset_arg = len(dataset_arg)
+    dm = ActiveDataModule(train_dataloader=dataloader)
+    len_dataset = len(dataloader.dataset)
 
-    ads.label(0)  # label first
-    assert ads.last_labelling_step == 1
-    assert ads.train_size == 1
-    assert ads.pool_size == len_dataset_arg - ads.train_size
-    assert ads.has_labelled_data is True
-    assert ads.has_unlabelled_data is True
-    assert ads.train_dataset.indices == [0]
+    dm.label(0)  # label first
+    assert dm.last_labelling_step == 1
+    assert dm.train_size == 1
+    assert dm.pool_size == len_dataset - dm.train_size
+    assert dm.has_labelled_data is True
+    assert dm.has_unlabelled_data is True
+    assert dm.train_dataset.indices == [0]
 
-    ads.label(list(range(len_dataset_arg - 1)))  # label the rest
-    assert ads.train_size == len_dataset_arg
-    assert ads.pool_size == len_dataset_arg - ads.train_size
-    assert ads.has_labelled_data is True
-    assert ads.has_unlabelled_data is False
-    assert ads.train_dataset.indices == list(range(len_dataset_arg))
+    dm.label(list(range(len_dataset - 1)))  # label the rest
+    assert dm.train_size == len_dataset
+    assert dm.pool_size == len_dataset - dm.train_size
+    assert dm.has_labelled_data is True
+    assert dm.has_unlabelled_data is False
+    assert dm.train_dataset.indices == list(range(len_dataset))
 
-    ads.reset_at_labelling_step(1)  # go back to when there was one instance
-    assert ads.train_size == 1
-    assert ads.pool_size == len_dataset_arg - ads.train_size
-    assert ads.has_labelled_data is True
-    assert ads.has_unlabelled_data is True
-    assert ads.train_dataset.indices == [0]
+    dm.reset_at_labelling_step(1)  # go back to when there was one instance
+    assert dm.train_size == 1
+    assert dm.pool_size == len_dataset - dm.train_size
+    assert dm.has_labelled_data is True
+    assert dm.has_unlabelled_data is True
+    assert dm.train_dataset.indices == [0]
 
-    ads.reset_at_labelling_step(0)  # go back to when there was nothing labelled
-    assert ads.last_labelling_step == 2
-    assert ads.train_size == 0
-    assert ads.pool_size == len_dataset_arg - ads.train_size
-    assert ads.has_labelled_data is False
-    assert ads.has_unlabelled_data is True
-    assert ads.train_dataset.indices == []
+    dm.reset_at_labelling_step(0)  # go back to when there was nothing labelled
+    assert dm.last_labelling_step == 2
+    assert dm.train_size == 0
+    assert dm.pool_size == len_dataset - dm.train_size
+    assert dm.has_labelled_data is False
+    assert dm.has_unlabelled_data is True
+    assert dm.train_dataset.indices == []
 
-    ads.reset_at_labelling_step(ads.last_labelling_step)  # reset to the last step
-    assert ads.train_size == len_dataset_arg
-    assert ads.pool_size == len_dataset_arg - ads.train_size
-    assert ads.has_labelled_data is True
-    assert ads.has_unlabelled_data is False
-    assert ads.train_dataset.indices == list(range(len_dataset_arg))
-
-    with pytest.raises(ValueError):
-        assert ads.reset_at_labelling_step(100)
-
-
-@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
-def test_sample_pool_indices(dataset_arg):
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
+    dm.reset_at_labelling_step(dm.last_labelling_step)  # reset to the last step
+    assert dm.train_size == len_dataset
+    assert dm.pool_size == len_dataset - dm.train_size
+    assert dm.has_labelled_data is True
+    assert dm.has_unlabelled_data is False
+    assert dm.train_dataset.indices == list(range(len_dataset))
 
     with pytest.raises(ValueError):
-        assert ads.sample_pool_idx(-1)
-
-    with pytest.raises(ValueError):
-        assert ads.sample_pool_idx(0)
-
-    with pytest.raises(ValueError):
-        assert ads.sample_pool_idx(ads.pool_size + 1)
-
-    assert len(ads.sample_pool_idx(ads.pool_size)) == ads.pool_size
-    assert len(ads.sample_pool_idx(1)) == 1
-
-
-@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
-def test_curriculum(dataset_arg):
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
-
-    for _ in range(5):
-        ads.label(0)
-
-    assert ads.curriculum_dataset().indices == list(range(5))
-
-
-@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
-def test_initial_labelling(dataset_arg):
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
-    assert ads.train_size == 0
-
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, initial_labels=[0])
-    assert ads.train_size == 1
-
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, initial_labels=2)
-    assert ads.train_size == 2
-
-    ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, initial_labels=2, val_split=0.5)
-    assert ads.train_size == ads.val_size == 1
-
-
-@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
-def test_dataloader_len(dataset_arg):
-    for batch_size in range(1, len(dataset_arg) + 1):
-        ads = ActiveDataModule(
-            num_classes=2,
-            train_dataset=dataset_arg,
-            initial_labels=2,
-            batch_size=batch_size,
-        )
-        assert ads.train_dataloader().batch_size is None
-        assert ads.train_dataloader().batch_sampler.batch_size == batch_size
-        assert len(ads.train_dataloader().batch_sampler) == len(ads.train_dataloader())
-
-    # min_steps_per_epoch
-    for shuffle in (True, False):
-        ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, initial_labels=2, shuffle=shuffle)
-        ads._min_steps_per_epoch = 1
-        assert len(ads.train_dataloader().batch_sampler) == len(ads.train_dataloader()) == 2
-        for _ in range(2):
-            assert next(iter(ads.train_dataloader()))
-
-        ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg, initial_labels=2, shuffle=shuffle)
-        ads._min_steps_per_epoch = 10
-        assert len(ads.train_dataloader().batch_sampler) == len(ads.train_dataloader()) == 10
-        for _ in range(10):
-            assert next(iter(ads.train_dataloader()))
-
-
-@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
-def test_sampler_type(dataset_arg):
-    ads = ActiveDataModule(
-        num_classes=2,
-        train_dataset=dataset_arg,
-        test_dataset=dataset_arg,
-        predict_dataset=dataset_arg,
-        val_dataset=dataset_arg,
-        initial_labels=2,
-        batch_size=1,
-    )
-
-    assert isinstance(ads.train_dataloader().batch_sampler.sampler, FixedLengthSampler)
-    assert isinstance(ads.pool_dataloader().batch_sampler.sampler, SequentialSampler)
-    assert isinstance(ads.val_dataloader().batch_sampler.sampler, SequentialSampler)
-    assert isinstance(ads.test_dataloader().batch_sampler.sampler, SequentialSampler)
-    assert isinstance(ads.predict_dataloader().batch_sampler.sampler, SequentialSampler)
-
-
-@pytest.mark.parametrize("dataset_arg", ["mock_dataset", "mock_hf_dataset"], indirect=True)
-def test_raise_errors(dataset_arg):
-    for i in (-0.5, 1.0):
-        with pytest.raises(MisconfigurationException):
-            ActiveDataModule(num_classes=2, train_dataset=dataset_arg, val_split=i)
-
-    with pytest.raises(MisconfigurationException):
-        ActiveDataModule(num_classes=2, train_dataset=dataset_arg, val_split=i, val_dataset=dataset_arg)
-
-    with pytest.raises(RuntimeError):
-        ads = ActiveDataModule(num_classes=2, train_dataset=dataset_arg)
-        next(iter(ads.train_dataloader()))
+        assert dm.reset_at_labelling_step(100)
